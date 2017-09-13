15f6176c94f631b8a91b1642f86b86e6cb165951
mlpbraga <mlpb@icomp.ufam.edu.br>
2017-09-13 09:47:01 -0400

    Comentadinho, e agora o texto considera a tag dos autores

--- teste.py
+++ teste.py
@@ -5,7 +5,8 @@ txt = arq.readlines()
 docs = []
 words = []
 
-interestedTags = ["TI", "MJ" , "MN" , "AB" , "EX"]
+interestedTags = ["AU","TI", "MJ" , "MN" , "AB" , "EX"]          #partes com informações interessantes do texto
+
 for linha in txt:
     linha = linha.strip("\n").split(" ")
     if( linha[0] != ""):
@@ -19,23 +20,24 @@ for linha in txt:
 
 hashWords = dict({})
 
-i = 1
-for doc in docs:
-    for palavra in doc:
-        if( not palavra in hashWords):
-            hashWords[palavra] = [(i , 1)]
-        else:
-            achou = False
-            lista = hashWords[palavra]
-            for k in range( len(lista)):
-                if( lista[k][0] == i):
-                    lista[k] = ( lista[k][0] , lista[k][1] + 1)
-                    achou = True
-                    break
-            if( not achou ):
-                lista.append( (i , 1))
-            hashWords[palavra] = lista
-    i+=1
+i = 1                                                           #i é o numero do documento atual que está sendo verificado
+
+for doc in docs:                                                #para cada documento da lista de documentos
+    for palavra in doc:                                         #para cada palavra no documento atual
+        if( not palavra in hashWords):                          #se a palavra não estiver no Hash ainda
+            hashWords[palavra] = [(i , 1)]                      #adiciona a palavra no hash por meio de uma tupla com documento i e frequencia 1
+        else:                                                   #senão, a palavra já estiver no Hash
+            achou = False                                       #não achou
+            lista = hashWords[palavra]                          #lista auxiliar recebe a lista da palavra atual no Hash
+            for k in range( len(lista)):                        #para cada tupla na lista
+                if( lista[k][0] == i):                          #se o primeiro elemento da tupla atual for igual ao i, a palavra já apareceu no documento antes
+                    lista[k] = ( lista[k][0] , lista[k][1] + 1) #então substituimos essa tupla por uma tupla com o mesmo i e a frequencia da tupla anterior +1
+                    achou = True                                #achou
+                    break                                       #fim
+            if( not achou ):                                    #se a palavra ainda não foi achada, ela ainda não tinha aparecido no documento antes, então
+                lista.append( (i , 1))                          #adicionamos na lista auxiliar uma tupla com o i atual e frquencia 1
+            hashWords[palavra] = lista                          #o Hash da palavra verificada recebe a lista auxiliar como lista definitiva
+    i+=1                                                        #incrementa i, pois veremos o próximo documento
 
 for x in hashWords:
     print( x  , " - " , hashWords[x])
